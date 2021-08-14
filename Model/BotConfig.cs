ebc0e7a98b87279197d8fde4d5993bc6ddc5bdd2
Yoshi 66 <yoshi66.tm@gmail.com>
2021-08-14 20:42:52 +0200

    Create structure

---
+++ Model/BotConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using Microsoft.Extensions.Logging;
+
+namespace DiscordServerUtilities.Model
+{
+	public class BotConfig
+	{
+		public string Token { get; private set; }
+		public LogLevel LogLevel { get; private set; }
+		public ulong AuthorID { get; private set; }
+		public ulong LoggingServerID { get; private set; }
+		public ulong LoggingChannelID { get; private set; }
+		public string DataBasePath { get; private set; }
+		public string Version { get; set; }
+	}
+}
